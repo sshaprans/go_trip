385c09c7587f13f0e818944bcf3be78783e4df1c
Serhii Shapran <sshaprans@gmail.com>
2022-11-21 00:51:56 +0200

    added full adaptability ver 1.01

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # go_trip
 ## Pet project, [show demo !](https://sshaprans.github.io/go_trip/)
 </br>
- HTML, CSS(FlexBox), JS(jquery)
+ HTML, CSS(FlexBox), JS(jquery, slider)
+ </br>
+ Adaptability (1270px, 1100px, 1000px, 850px, 640px, 450px)
--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,19 @@ const owl = $('.owl-carousel');
 owl.owlCarousel({
     center: true,
     loop: true,
-    margin: 30,
-    items:3,
-    startPosition: 1, 
+    margin: 15,
+    startPosition: 1,
+    items: 1, 
     responsive : {
         640 : {
-            margin: 15,
+            items: 3, 
         },
         850 : {
-            margin: 15,
-        },
-        1000 : {
             margin: 20,
+            items: 3, 
         },
         1200 : {
+            items: 3, 
             margin: 30,
         }
     }
