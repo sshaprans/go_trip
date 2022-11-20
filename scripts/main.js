8f026d340f2b09ebc00be171b727eb71c3483b9e
Serhii Shapran <sshaprans@gmail.com>
2022-11-20 22:33:06 +0200

    add adaptability

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,26 @@
+// slider
 const owl = $('.owl-carousel');
 
 owl.owlCarousel({
     center: true,
     loop: true,
     margin: 30,
-    item:3,
+    items:3,
     startPosition: 1, 
+    responsive : {
+        640 : {
+            margin: 15,
+        },
+        850 : {
+            margin: 15,
+        },
+        1000 : {
+            margin: 20,
+        },
+        1200 : {
+            margin: 30,
+        }
+    }
 });
 
 $('.slider__btn--r').click(function() {
@@ -16,3 +31,14 @@ $('.slider__btn--l').click(function() {
     owl.trigger('next.owl.carousel');
 })
 
+
+// menu icon
+const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function(){
+    nav.classList.toggle('nav--mobile')
+    menuIcon.classList.toggle('menu-icon-active');
+    document.body.classList.toggle('no-scroll');
+}
